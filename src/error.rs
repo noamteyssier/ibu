cdcf0ff5a1632b804f5d2bbf48796f37ed6de76d
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2024-12-03 15:14:18 -0800

    feat: added a better error

---
+++ src/error.rs
@@ -0,0 +1,22 @@
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum BinaryFormatError {
+    #[error("IO error: {0}")]
+    Io(#[from] std::io::Error),
+
+    #[error("Invalid version {0} in header")]
+    InvalidVersion(u32),
+
+    #[error("Invalid barcode length {0}")]
+    InvalidBarcodeLength(u32),
+
+    #[error("Invalid UMI length {0}")]
+    InvalidUMILength(u32),
+
+    #[error("Attempted to read record from empty or corrupted data")]
+    InvalidRecord { actual: usize },
+
+    #[error("Record values exceed maximum allowed by header specification")]
+    RecordValueOverflow { barcode_len: u32, umi_len: u32 },
+}
