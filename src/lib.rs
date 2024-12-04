c7c73ff13c52b310d9ab6ae1ee4c5f3ad61926f1
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2024-12-04 09:52:08 -0800

    chore: make constants public

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,17 @@ pub use constructs::{Header, Record};
 pub use error::BinaryFormatError;
 pub use io::{Reader, Writer};
 
-const VERSION: u32 = 1;
-const MAX_BARCODE_LEN: u32 = 32;
-const MAX_UMI_LEN: u32 = 32;
+// Version of the binary format
+pub const VERSION: u32 = 1;
+
+// Maximum lengths for barcode (assumes 2bit encoding)
+pub const MAX_BARCODE_LEN: u32 = 32;
+
+// Maximum lengths for UMI (assumes 2bit encoding)
+pub const MAX_UMI_LEN: u32 = 32;
 
 // Size of the header in bytes
-const SIZE_HEADER: usize = 13;
+pub const SIZE_HEADER: usize = 13;
 
 // Size of a record in bytes
-const SIZE_RECORD: usize = 24;
+pub const SIZE_RECORD: usize = 24;
